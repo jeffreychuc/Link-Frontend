1e34e08adcffa3dd3da7a0e2dd19e5a772b50308
Jeffrey Chuc <jeffreychuc@gmail.com>
2018-01-28 00:16:38 -0800

    add about section

--- frontend/components/homepage.jsx
+++ frontend/components/homepage.jsx
@@ -3,11 +3,13 @@ import NavHeader from './navheader_container';
 import Splash from './splash_container';
 import SampleInputs from './sample_inputs_container';
 import MapDisplay from './mapdisplay_container';
+import About from './about_container';
 
 const Landing = () => (
   <div className='landing'>
     <NavHeader />
     <Splash />
+    <About />
     <SampleInputs />
     <MapDisplay />
   </div>
